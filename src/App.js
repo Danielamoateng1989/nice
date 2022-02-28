9bfe31510e7faa4c77fb2d1eb5bf1253d2dce172
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-28 02:54:34 -0500

    add servicelist page to app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './index.css';
 import Navbar from './Components/Header/Navbar';
 import Footer from './Components/Footer/Footer';
+import Homepage from './Pages/Home/ServicesList';
 
 function App() {
   return (
     <>
    <Navbar />
    <main>
-
+    <Homepage/>
    </main>
    <Footer />
    </>
---
+++ src/Pages/Home/ServicesList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import services from '../../services'
+
+const ServicesList = () => {
+    return (
+        <div>
+            <h1>ServicesList</h1>
+        </div>
+    )
+}
+
+export default ServicesList
