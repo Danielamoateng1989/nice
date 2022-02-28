271acbd52209e1751ca09cd5b3384db8ff866e76
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-27 21:32:27 -0500

    add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './index.css';
 import Navbar from './Components/Header/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
+    <>
    <Navbar />
+   <main>
+
+   </main>
+   <Footer />
+   </>
   );
 }
 
