de4074053d29504267412da3d72a41f3f8f3aa4f
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-28 03:03:53 -0500

    add service card module

---
+++ src/Components/Homepage/ServicesList.jsx
@@ -0,0 +1,80 @@
+import {
+    AspectRatio,
+    Box,
+    HStack,
+    Image,
+    Skeleton,
+    Stack,
+    Text,
+    useBreakpointValue,
+    useColorModeValue,
+} from '@chakra-ui/react'
+import { Rating } from './Rating'
+import FavoriteButton from './FavoriteButton'
+// import { Link } from 'react-router-dom'
+
+
+
+export const ServicesCard = (props) => {
+    const { rootProps } = props
+    return (
+        <div className="content">
+            <Stack
+                spacing={useBreakpointValue({
+                    base: '2',
+                    md: '3',
+                })}
+                {...rootProps}
+            >
+                <Box position="relative">
+
+                    {/* <Link to={`/services/${props.product._id}`}> */}
+                        <AspectRatio ratio={4 / 3}>
+
+                            <Image
+                                src={props.service.image}
+                                alt={props.service.name}
+                                cursor="pointer"
+                                draggable="false"
+                                fallback={<Skeleton />}
+                                borderRadius={useBreakpointValue({
+                                    base: 'md',
+                                    md: 'xl',
+                                })}
+                                objectFit="cover"
+                            />
+
+                        </AspectRatio>
+                    {/* </Link> */}
+                    <FavoriteButton
+                        position="absolute"
+                        top="4"
+                        right="4"
+                        aria-label={`Add ${props.service.name} to your favourites`}
+                    />
+                </Box>
+                <Stack>
+                    <Stack spacing="1">
+                        <Text fontWeight="md" fontSize="md" color={"gray"}>
+                            {props.service.name}
+                        </Text>
+                        {props.service.price}
+                    </Stack>
+                    <HStack>
+                        <Text fontSize="xl" color={"black"} fontWeight="bold">
+                            ${props.service.price}
+                        </Text>
+                    </HStack>
+                    <HStack>
+                        <Rating defaultValue={props.product.rating} size="sm" />
+                        <Text fontSize="sm" color={useColorModeValue('gray.600', 'gray.400')}>
+                            ({props.service.numberOfReviews} reviews)
+          </Text>
+                    </HStack>
+                </Stack>
+            </Stack>
+
+        </div>
+
+    )
+}
