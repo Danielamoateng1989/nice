b0dd426a026917470a2165280b45deca18030895
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-28 13:39:12 -0500

    increase font-weight for price, change font color to black

--- src/Pages/ServicesDetailPage/ServicesDetail.jsx
+++ src/Pages/ServicesDetailPage/ServicesDetail.jsx
@@ -7,7 +7,7 @@ import {
     Stack,
     HStack,
     Text,
-   
+    useColorModeValue
 } from '@chakra-ui/react'
 import React from 'react'
 import { Link, useLocation} from 'react-router-dom'
@@ -38,8 +38,131 @@ const ServicesDetail = () => {
     
     return (
      <React.Fragment>
+        <Box
+          maxW="7xl"
+          mx="auto"
+          px={{
+            base: '0',
+            lg: '12',
+          }}
+          py={{
+            base: '0',
+            lg: '12',
+          }}
+        >
+          <Link to="/">
+            <button type="button"
+              className="back-btn"
+            >Go back</button>
+          </Link>
+          <Stack
+            direction={{
+              base: 'column-reverse',
+              lg: 'row',
+            }}
+            spacing={{
+              base: '0',
+              lg: '20',
+            }}
+          >
+            <Box
+              width={{
+                lg: 'sm',
+              }}
+              transform={{
+                base: 'translateY(-50%)',
+                lg: 'none',
+              }}
+              bg={{
+                base: useColorModeValue('gray.50', 'gray.700'),
+                lg: 'transparent',
+              }}
+              mx={{
+                base: '6',
+                md: '8',
+                lg: '0',
+              }}
+              px={{
+                base: '6',
+                md: '8',
+                lg: '0',
+              }}
+              py={{
+                base: '6',
+                md: '8',
+                lg: '12',
+              }}
+            >
 
-      <h2>{service.name}</h2>
+              <Stack
+                spacing={{
+                  base: '8',
+                  lg: '10',
+                }}
+              >
+                <Stack
+                  spacing={{
+                    base: '2',
+                    lg: '4',
+                  }}
+                >
+
+                  <h1 size="xl" color={"black"} className="hero-heading">
+                    {service.name}
+                  </h1>
+                  <p size="xl" fontWeight="normal" className="price">
+                    ${service.price}
+                  </p>
+                  <HStack>
+                    <Rating defaultValue={service.rating} size="sm" />
+                    {service.rating}
+                    <Text fontSize="sm" color={useColorModeValue('gray.600', 'gray.400')}>
+                      ({service.numberOfReviews} reviews)
+          </Text>
+                  </HStack>
+                  <HStack />
+                  {/* <Button onClick={() => navigate('/dateandtime')}>Book Now</Button> */}
+                </Stack>
+
+              </Stack>
+
+            </Box>
+
+            <Flex flex="1" overflow="hidden">
+
+              <Image
+                src={service.image}
+                alt="Lovely Image"
+                fallback={<Skeleton />}
+                maxH="450px"
+                minW="300px"
+                objectFit="cover"
+                flex="1"
+                borderRadius="15"
+              />
+
+            </Flex>
+
+          </Stack>
+
+        </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
      </React.Fragment>
     )
