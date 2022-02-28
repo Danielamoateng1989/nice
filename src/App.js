a5ec1b672413f1e29ba8162843052b62ebfd4b9c
Daniel Amoateng <danielamoateng81@yahoo.com>
2022-02-28 04:13:38 -0500

    fix hero image

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './index.css';
 import Navbar from './Components/Header/Navbar';
 import Footer from './Components/Footer/Footer';
 import Homepage from './Pages/Home/Services';
+import Hero from './Components/Hero/Hero';
 
 function App() {
   return (
     <>
    <Navbar />
    <main>
+     <Hero />
     <Homepage/>
    </main>
    <Footer />
